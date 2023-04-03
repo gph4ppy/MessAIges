9778470e3d7d3dab9951836e6c9867ffeffc5a8a
Jakub Dąbrowski <jakub.dabrowski.dev@icloud.com>
2023-04-03 20:53:42 +0200

    Initial Commit

---
+++ MessAIges/MessAIgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessAIgesApp.swift
+//  MessAIges
+//
+//  Created by Jakub Dąbrowski on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessAIgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
