3d028893d1114ae3ec912642a280fde550d729f0
Jakub Dąbrowski <jakub.dabrowski.dev@icloud.com>
2023-04-03 21:42:58 +0200

    Add HistoryViewModel

---
+++ MessAIges/Source Files/Application Flow/History/ViewModels/HistoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryViewModel.swift
+//  MessAIges
+//
+//  Created by Jakub Dąbrowski on 03/04/2023.
+//
+
+import Foundation
+
+@MainActor
+final class HistoryViewModel: ObservableObject {
+
+}
